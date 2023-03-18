d0a78a0382c3e9e487411157f583097e662c33b9
Rubies21 <jadesolaadeagbo@gmail.com>
2023-03-18 15:07:22 +0100

    More changes made to connect wallet button

--- src/components/WalletButton.js
+++ src/components/WalletButton.js
@@ -3,7 +3,42 @@ import "./Nav.css"
 
 
 const WalletButton = () => {
-  return <ConnectButton/>;
+  return (
+    <>
+      <ConnectButton.Custom>
+            {({
+              account,
+              chain,
+              openAccountModal,
+              openConnectModal,
+              authenticationStatus,
+              mounted,
+            }) => {
+              // Note: If your app doesn't use authentication, you
+              // can remove all 'authenticationStatus' checks
+              const ready = mounted && authenticationStatus !== "loading";
+              const connected =
+                ready &&
+                account &&
+                chain &&
+                (!authenticationStatus ||
+                  authenticationStatus === "authenticated");
+
+              return (
+                <button
+                  onClick={() =>
+                    connected ? openAccountModal() : openConnectModal()
+                  }
+                  className={`butt ${connected ? "connectedbutt":""}`}
+                >
+                  {connected ? account.displayName : "Connect Wallet"}
+
+                </button>
+              );
+            }}
+          </ConnectButton.Custom>
+    </>
+  )
 }; 
 
 export default WalletButton
